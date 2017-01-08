598f826002f7d546325cca9d556fe254ea839408
Gassan Idriss <ghassani@gmail.com>
2017-01-07 23:51:42 -0800

    Auto

--- include/qualcomm/mbn_parser.h
+++ include/qualcomm/mbn_parser.h
@@ -13,44 +13,150 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
+#include <vector>
+#include <memory>
 #include "definitions.h"
 #include "qualcomm/mbn.h"
 
 namespace OpenPST {
 	namespace QC {
 
-		struct MbnHeader {
-			bool is80byte;
-			union {
-				MbnHeader80byte header80;
-				MbnHeader40byte header40;
-			};
+		enum MbnParserFlag {
+			kMbnParserFlagNone 			   = 0x00,
+			kMbnParserFlagExtractCode 	   = 0x01,
+			kMbnParserFlagExtractSignature = 0x02,
+			kMbnParserFlagExtractX509 	   = 0x04,
+			kMbnParserFlagAll		   	   = kMbnParserFlagExtractCode | 
+											kMbnParserFlagExtractSignature | kMbnParserFlagExtractX509
 		};
 
-		class MbnParser {
-			
+		class Mbn {
 			protected:
 				std::string filePath;
-				size_t fileSize = 0;
-				MbnHeader header = {};
+				std::string imageName;
+				int parserFlags;
+				size_t fileSize;
+				uint32_t imageId = 0;
+				uint32_t imageSrc = 0;
+				uint32_t imageDestPtr = 0;
+				uint32_t imageSize = 0;
+				uint32_t codeSize = 0;
+				uint32_t signaturePtr = 0;
+				uint32_t signatureSize = 0;
+				uint32_t certChainPtr = 0;
+				uint32_t certChainSize = 0;
+				uint32_t oemRootCertSelection = 0;
+				uint32_t oemNumberOfRootCerts = 0;
+			public:
+				std::vector<uint8_t> header;
+				std::vector<uint8_t> code;
+				std::vector<uint8_t> signature;
+				std::vector<uint8_t> x509;
+
+				Mbn(std::string filePath, int parserFlags);
+				
+				~Mbn();
+				
+				const std::string& getFilePath();
+				
+				int getParserFlags();
+
+				size_t getFileSize();
+
+				void setFileSize(size_t fileSize);
+
+				std::string& getImageName();
+
+				void setImageName(std::string imageName);
+
+				uint32_t getImageId();
+
+				void setImageId(uint32_t imageId);
+
+				uint32_t getImageDestPtr();
+
+				void setImageDestPtr(uint32_t imageDestPtr);
+
+				uint32_t getImageSize();
+
+				void setImageSize(uint32_t imagSize);
+
+				uint32_t getCodeSize();
+
+				void setCodeSize(uint32_t codeSize);
+
+				uint32_t getSignaturePtr();
+
+				void setSignaturePtr(uint32_t signaturePtr);
+
+				uint32_t getSignatureSize();
+
+				void setSignatureSize(uint32_t signatureSize);
+
+				uint32_t getOemRootCertSelection();
+
+				void setOemRootCertSelection(uint32_t oemRootCertSelection);
+
+				uint32_t getOemNumberOfRootCerts();
+
+				void setOemNumberOfRootCerts(uint32_t oemNumberOfRootCerts);
+
+				void setImageSrc(uint32_t imageSrc);
+
+				uint32_t getImageSrc();
+	
+				void setCertChainPtr(uint32_t certChainPtr);
+
+				uint32_t getCertChainPtr();
+
+				void setCertChainSize(uint32_t certChainSize);
+
+				uint32_t getCertChainSize();
+
+				std::vector<uint8_t>& getHeader();
+
+				std::vector<uint8_t>& getCode();
+
+				std::vector<uint8_t>& getSignature();
+
+				std::vector<uint8_t>& getX509();
+		};
+			
+		class MbnParser {
+			
 			public:
 				/**
 				* @brief - Constructor
 				*/
-				MbnParser(std::string filePath);
+				MbnParser();
 
 				/**
 				* @brief - Deconstructor
 				*/
 				~MbnParser();
 
-				const MbnHeader& getHeader();
+				std::unique_ptr<Mbn> parse(std::string filePath, int flags = kMbnParserFlagNone);
 
-				int getHeaderSize();
-
-				std::string getImageName();
-				
 				std::string getImageName(uint32_t imageId);
+
+			protected:
+				size_t readSegment(std::ifstream& file, int offset, int size, size_t maxSize, std::vector<uint8_t>& dest);
+
 		};
+
+		class MbnParserException : public std::exception
+		{
+			const MbnParserException& operator=(MbnParserException);
+			std::string _what;
+			public:
+				MbnParserException(std::string message) : _what(message)  { }
+				MbnParserException(const MbnParserException& second) : _what(second._what) {}
+				virtual ~MbnParserException() throw() {}
+				virtual const char* what() const throw () {
+					return _what.c_str();
+				}
+
+		};		
 	}
 }
--- src/qualcomm/mbn_parser.cpp
+++ src/qualcomm/mbn_parser.cpp
@@ -13,30 +13,180 @@
 
 using namespace OpenPST::QC;
 
-MbnParser::MbnParser(std::string filePath) : filePath(filePath)
+Mbn::Mbn(std::string filePath, int parserFlags) : filePath(filePath), parserFlags(parserFlags)
 {
-	std::stringstream ss;
-	std::ifstream file(filePath.c_str(), std::ios::in | std::ios::binary);
-				
 
-	if (!file.is_open()) {
-		ss << "Could Not Open File " << filePath;
-		throw std::invalid_argument(ss.str());
-	}
+}
 
-	file.seekg(0, file.end);
-	fileSize = (size_t)file.tellg();
-	file.seekg(0, file.beg);
+Mbn::~Mbn()
+{
 
-	if (fileSize < sizeof(header.header80)) {
-		ss << "Invalid File Type. File is smaller than header. " << filePath;
-		throw std::invalid_argument(ss.str());
-	}
+}
 
-	file.read(reinterpret_cast<char*>(&header.header80), sizeof(header.header80));
-	file.close();
+const std::string& Mbn::getFilePath()
+{
+	return filePath;
+}
 
-	header.is80byte = header.header80.magic == MBN_EIGHTY_BYTE_MAGIC;
+int Mbn::getParserFlags()
+{
+	return parserFlags;
+}
+
+size_t Mbn::getFileSize()
+{
+	return fileSize;
+}
+
+void Mbn::setFileSize(size_t _fileSize)
+{
+	fileSize = _fileSize;
+}
+
+std::string& Mbn::getImageName()
+{
+	return imageName;
+}
+
+void Mbn::setImageName(std::string _imageName)
+{
+	imageName = _imageName;
+}
+
+uint32_t Mbn::getImageId()
+{
+	return imageId;
+}
+
+void Mbn::setImageId(uint32_t _imageId)
+{
+	imageId = _imageId;
+}
+
+uint32_t Mbn::getImageDestPtr()
+{
+	return imageDestPtr;
+}
+
+void Mbn::setImageDestPtr(uint32_t _imageDestPtr)
+{
+	imageDestPtr = _imageDestPtr;
+}
+
+uint32_t Mbn::getImageSize()
+{
+	return imageSize;
+}
+
+void Mbn::setImageSize(uint32_t _imageSize)
+{
+	imageSize = _imageSize;
+}
+
+uint32_t Mbn::getCodeSize()
+{
+	return codeSize;
+}
+
+void Mbn::setCodeSize(uint32_t _codeSize)
+{
+	codeSize = _codeSize;
+}
+
+uint32_t Mbn::getSignaturePtr()
+{
+	return signaturePtr;
+}
+
+void Mbn::setSignaturePtr(uint32_t _signaturePtr)
+{
+	signaturePtr = _signaturePtr;
+}
+
+uint32_t Mbn::getSignatureSize()
+{
+	return signatureSize;
+}
+
+void Mbn::setSignatureSize(uint32_t _signatureSize)
+{
+	signatureSize = _signatureSize;
+}
+
+uint32_t Mbn::getOemRootCertSelection()
+{
+	return oemRootCertSelection;
+}
+
+void Mbn::setOemRootCertSelection(uint32_t _oemRootCertSelection)
+{
+	oemRootCertSelection = _oemRootCertSelection;
+}
+
+uint32_t Mbn::getOemNumberOfRootCerts()
+{
+	return oemNumberOfRootCerts;
+}
+
+void Mbn::setOemNumberOfRootCerts(uint32_t _oemNumberOfRootCerts)
+{
+	oemNumberOfRootCerts = _oemNumberOfRootCerts;
+}
+
+
+std::vector<uint8_t>& Mbn::getHeader()
+{
+	return header;
+}
+
+std::vector<uint8_t>& Mbn::getCode()
+{
+	return code;
+}
+
+std::vector<uint8_t>& Mbn::getSignature()
+{
+	return signature;
+}
+
+std::vector<uint8_t>& Mbn::getX509()
+{
+	return x509;
+}
+
+void Mbn::setImageSrc(uint32_t _imageSrc)
+{
+	imageSrc = _imageSrc;
+}
+
+uint32_t Mbn::getImageSrc()
+{
+	return imageSrc;
+}
+
+void Mbn::setCertChainPtr(uint32_t _certChainPtr)
+{
+	certChainPtr = _certChainPtr;
+}
+
+uint32_t Mbn::getCertChainPtr()
+{
+	return certChainPtr;
+}
+
+void Mbn::setCertChainSize(uint32_t _certChainSize)
+{
+	certChainSize = _certChainSize;
+}
+
+uint32_t Mbn::getCertChainSize()
+{
+	return certChainSize;
+}
+
+MbnParser::MbnParser()
+{
+
 }
 
 MbnParser::~MbnParser()
@@ -44,25 +194,137 @@ MbnParser::~MbnParser()
 
 }
 
-const MbnHeader& MbnParser::getHeader()
+std::unique_ptr<Mbn> MbnParser::parse(std::string filePath, int flags)
 {
-	return header;
-}
+	uint32_t magic;
+	std::stringstream ss;
+	char headerBuff[MBN_HEADER_MAX_SIZE] = {};
+	std::ifstream file(filePath.c_str(), std::ios::in | std::ios::binary);
 
-int MbnParser::getHeaderSize()
-{
-	return header.is80byte ? 
-		sizeof(MbnHeader80byte) : sizeof(MbnHeader40byte);
-}
-
-std::string MbnParser::getImageName()
-{
-	if (header.is80byte) {
-		return getImageName(header.header80.image_id);
-	} else {
-		return getImageName(header.header40.image_id);
+	if (!file.is_open()) {
+		ss << "Could Not Open File " << filePath;
+		throw MbnParserException(ss.str());
 	}
+
+	std::unique_ptr<Mbn> ret(new Mbn(filePath, flags));
+
+	file.seekg(0, file.end);
+
+	ret->setFileSize((size_t)file.tellg());
+
+	file.seekg(0, file.beg);
+
+	if (ret->getFileSize() < MBN_HEADER_MAX_SIZE) {
+		ss << "Invalid File Type. File " << filePath << " is smaller than max header size";
+		throw MbnParserException(ss.str());
+	}
+
+	// seek to 80 byte magic read it
+	file.seekg(sizeof(uint32_t), file.cur);
+
+	file.read(reinterpret_cast<char*>(&magic), sizeof(magic));
+
+	file.seekg(0, file.beg);
+
+	int headerSize = 0;
+
+	bool is80ByteHeader = magic == MBN_EIGHTY_BYTE_MAGIC;
+
+	if (is80ByteHeader) {
+		headerSize = sizeof(MbnHeader80byte);
+		file.read(reinterpret_cast<char*>(headerBuff), headerSize);
+	} else {
+		// 40 byte has no magic		
+		headerSize = sizeof(MbnHeader40byte);
+		file.read(reinterpret_cast<char*>(headerBuff), headerSize);
+	}
+
+	ret->header.reserve(headerSize);
+
+	ret->header.insert(
+		ret->header.begin(), 
+		headerBuff, 
+		headerBuff + headerSize
+	);
+
+	if (is80ByteHeader) {
+		MbnHeader80byte* header = (MbnHeader80byte*)&ret->header[0];
+		ret->setImageId(header->image_id);
+		ret->setImageName(getImageName(header->image_id));
+		ret->setImageDestPtr(header->image_dest_ptr);
+		ret->setImageSize(header->image_size);
+		ret->setCodeSize(header->code_size);
+		ret->setSignaturePtr(header->signature_ptr);
+		ret->setSignatureSize(header->signature_size);
+		ret->setOemRootCertSelection(header->oem_root_cert_sel);
+		ret->setOemNumberOfRootCerts(header->oem_num_root_certs);
+		ret->setImageSrc(header->image_src);
+		ret->setCertChainPtr(header->cert_chain_ptr);
+		ret->setCertChainSize(header->cert_chain_size);
+	} else {
+		MbnHeader40byte* header = (MbnHeader40byte*)&ret->header[0];
+		ret->setImageId(header->image_id);
+		ret->setImageName(getImageName(header->image_id));
+		ret->setImageDestPtr(header->image_dest_ptr);
+		ret->setImageSize(header->image_size);
+		ret->setCodeSize(header->code_size);
+		ret->setSignaturePtr(header->signature_ptr);
+		ret->setSignatureSize(header->signature_size);
+		ret->setOemRootCertSelection(0);
+		ret->setOemNumberOfRootCerts(0);
+		ret->setImageSrc(header->image_src);
+		ret->setCertChainPtr(header->cert_chain_ptr);
+		ret->setCertChainSize(header->cert_chain_size);
+	}
+	
+	int offset, size;
+
+	if (flags & kMbnParserFlagExtractCode){
+		offset = ret->getImageSrc();
+		size   = ret->getCodeSize();
+		readSegment(file, offset, size, ret->getFileSize(), ret->code);
+	}
+	
+	if (flags & kMbnParserFlagExtractSignature) {
+		offset = ret->getImageSrc() + ret->getCodeSize();
+		size   = ret->getSignatureSize();				
+		readSegment(file, offset, size, ret->getFileSize(), ret->signature);
+	}
+	
+	if (flags & kMbnParserFlagExtractX509) {
+		offset 	= ret->getImageSrc() + ret->getCodeSize() + ret->getSignatureSize();
+		size   	= ret->getCertChainSize();
+		readSegment(file, offset, size, ret->getFileSize(), ret->x509);
+	}
+
+	file.close();
+
+	return std::move(ret);
 }
+
+size_t MbnParser::readSegment(std::ifstream& file, int offset, int size, size_t maxSize, std::vector<uint8_t>& dest)
+{
+	if (size > maxSize) {
+		std::stringstream ss;
+		ss  << "Requested offset exceeds max size for segment at " << offset << " but max size is " << maxSize << std::endl;
+		throw MbnParserException(ss.str());
+	}
+
+	char* tmp = new char[size];
+
+	dest.reserve(size);
+
+	file.seekg(offset, file.beg);
+
+	file.read(tmp, size);
+
+	dest.insert(dest.begin(), tmp, tmp + size);
+
+	delete[] tmp;
+
+	return dest.size();
+}
+
 
 std::string MbnParser::getImageName(uint32_t imageId)
 {
